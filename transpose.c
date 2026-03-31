ad539073f4a0dbf5915dfdd36d57ac8e50892d26
Chanchal Dharmani <25dharmanic@rbunagpur.in>
2026-03-31 23:19:14 +0530

    Add matrix transpose functionality in transpose.c

---
+++ transpose.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    int matrix[3][3],transpose[3][3];
+    printf("Elements of matrix:");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",&matrix[i][j]);
+        }
+    }
+    printf("Matrix:\n");
+     for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d\t",matrix[i][j]);
+        }
+        printf("\n");
+    }  
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            transpose[j][i]=matrix[i][j];
+        }
+    }
+    printf("Transpose of matrix:\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d\t",transpose[i][j]);
+        }    
+        printf("\n");
+    }
+    return 0;    
+}
